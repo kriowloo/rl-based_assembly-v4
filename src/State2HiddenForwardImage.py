35d0a664522fc7f81db3d672322f396ad4f92f5d
kriowloo <43251653+kriowloo@users.noreply.github.com>
2019-09-26 18:09:45 -0300

    Update State2HiddenForwardImage.py

--- src/State2HiddenForwardImage.py
+++ src/State2HiddenForwardImage.py
@@ -8,12 +8,12 @@ class State2HiddenForwardImage(State2ForwardImage):
     def getStateInfoForReads(self, read_ids_order):
         image1, pm1 = super().getStateInfoForReads(read_ids_order)
         hide = False
-        for i in range(len(image1)):
-            if hide:
-                image1[i][0] = -1
-                image1[i][1] = None
-            elif i > 0 and image1[i][0] == 0:
+        for i in range(1,len(image1)):
+            if not hide and image1[i][0] == 0:
                 aux = min(len(image1[i][1]), image1[i-1][1])
                 if image1[i][1][:aux] != image1[i-1][1][:aux]:
                     hide = True
+            if hide:
+                image1[i][0] = -1
+                image1[i][1] = None
         return [image1], pm1
