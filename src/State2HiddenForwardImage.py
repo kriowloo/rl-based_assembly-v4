7771955a9b63ce9592f7b3d5ef17267e88e9ecc4
kriowloo <43251653+kriowloo@users.noreply.github.com>
2019-09-26 18:34:23 -0300

    Update State2HiddenForwardImage.py

--- src/State2HiddenForwardImage.py
+++ src/State2HiddenForwardImage.py
@@ -11,7 +11,7 @@ class State2HiddenForwardImage(State2ForwardImage):
         hide = False
         for i in range(1,len(image1)):
             if not hide and image1[i][0] == 0:
-                aux = min(len(image1[i][1]), image1[i-1][1])
+                aux = min(len(image1[i][1]), len(image1[i-1][1]))
                 if image1[i][1][:aux] != image1[i-1][1][:aux]:
                     hide = True
             if hide:
