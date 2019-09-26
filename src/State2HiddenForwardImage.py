d7e0a01fd2c28aa4070d51872802a75bed1d1388
kriowloo <43251653+kriowloo@users.noreply.github.com>
2019-09-26 17:37:18 -0300

    Update State2HiddenForwardImage.py

--- src/State2HiddenForwardImage.py
+++ src/State2HiddenForwardImage.py
@@ -1,21 +1,12 @@
-from State2Image import State2Image
+from State2Image import State2ForwardImage
 
-class State2HiddenForwardImage(State2Image):
+class State2HiddenForwardImage(State2ForwardImage):
     def __init__(self, reads, max_read_len, match, mismatch, gap, n_reads):
         super().__init__(match, mismatch, gap, reads, max_read_len, n_reads)
-        self.init = None
-
-    # return two empty images that correspond to the initial state of
-    # state space
-    def getInitialState(self):
-        if self.init is None:
-            aux = State2Image.getEmptyCompressedImage(self.image_height)
-            self.init = [aux]
-        return self.init
 
     # return two information (1- image(s) representation for the reads and 2- PM for the reads)
     def getStateInfoForReads(self, read_ids_order):
-        image1, pm1 = self._getCompressedImageForReads(read_ids_order)
+        image1, pm1 = super().getStateInfoForReads(read_ids_order)
         hide = False
         for i in range(len(image1)):
             if hide:
