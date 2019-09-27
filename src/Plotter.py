b84d05cdb31abcd9b67c24fff2301d220702addf
kriowloo <43251653+kriowloo@users.noreply.github.com>
2019-09-27 16:01:29 -0300

    Update Plotter.py

--- src/Plotter.py
+++ src/Plotter.py
@@ -1,21 +1,25 @@
 import matplotlib.pyplot as plt
 
 class Plotter:
-    def __init__(self, maxPM, output_path):
+    def __init__(self, maxPM, output_path, title = None):
         self.maxPM = maxPM
         self.output_path = output_path
+        self.title = title
         self.clearBuffer()
-        
+
     def clearBuffer(self):
         self.pms = []
         self.epsilons = []
-        
+
     def addPoint(self, pm, epsilon):
         self.epsilons.append(epsilon)
         self.pms.append(pm / self.maxPM)
-        
+
     def plotPerformance(self):
         x = range(len(self.epsilons))
-        plt.plot(x, self.epsilons)
-        plt.plot(x, self.pms)
+        plt.plot(x, self.epsilons, label='Epsilon')
+        plt.plot(x, self.pms, label='Relative PM')
+        plt.legend()
+        if self.title is not None:
+            plt.suptitle(self.title)
         plt.savefig(self.output_path)
