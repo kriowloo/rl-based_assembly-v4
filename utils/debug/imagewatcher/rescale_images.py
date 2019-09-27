e957fb47e840474e20457a38ac3d1197d661a0f4
kriowloo <43251653+kriowloo@users.noreply.github.com>
2019-09-26 21:09:20 -0300

    Create rescale_images.py
    Script that rescales images and draws the corresponding score to produce video

---
+++ utils/debug/imagewatcher/rescale_images.py
@@ -0,0 +1,39 @@
+import glob
+import sys
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+def watch(score, input_path, scale, output_path, text_width_offset = None, font_size = 100):
+    font = ImageFont.truetype("DejaVuSansMono-Bold.ttf", font_size)
+    img = Image.open(input_path)
+    width, height = img.size
+    width *= scale
+    height *= scale
+    img = img.resize((width, height), Image.NEAREST)
+    draw = ImageDraw.Draw(img)
+    if text_width_offset is None:
+        text_width_offset = font.getsize(text)[0]
+    score = "%.2f" % round(score,2)
+    draw.text((width - text_width_offset, 0),score,font=font)
+    img.save(output_path)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 6:
+        print("Usage: python3 " + sys.argv[0] + " <font_size> <scale> <prefix> <extension> <order>")
+        print("Example: python3 " + sys.argv[0] + " 100 20 rescaled png 1,2,3")
+        print("         - order: 1=episode, 2=action in episode, 3=pm")
+        sys.exit(1)
+    font_size = int(sys.argv[1])
+    scale = int(sys.argv[2])
+    prefix = sys.argv[3]
+    extension = sys.argv[4]
+    order = [int(x)-1 for x in sys.argv[5].split(",")]
+    for file in glob.glob('*.' + extension):
+        info = file.split("_")[1:4]
+        output_path = prefix
+        for x in order:
+            output_path += "_" + info[x]
+        output_path += "." + extension
+        score = int(info[2]) / 100.0
+        watch(score, file, scale, output_path, font_size)
